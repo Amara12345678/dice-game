9a56ec1d86f345d3c9d63d107609f8ec2d37bf0c
amaraa <moonkeymon924@gmail.com>
2022-10-20 09:24:02 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ var scores = [0, 0];
 //toglogchiin eeljindee tsugluulj baigaa onoog hadgalah
 var roundScore = 0;
 //shoonii ali talaaraa buusniig hadgalah huwisagch 1ees 6 huwshagchiig sanamsarguuger vvsgej ugnu
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // document.querySelector("#score-0").textContent = dice;
 
 // document.querySelector("#score-1").textContent = dice;
 
 //togloom ehlehed beltgey
-document.querySelector("#score-0").textContent = 0;
+document.getElementById("score-0").textContent = "0";
 
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
+document.getElementById("current-0").textContent = "0";
 
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
 
-console.log("шоо :" + dice);
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
